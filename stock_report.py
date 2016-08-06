35926c18975d40633d381624b6915b5a487e58b6
jason <ph3world@163.com>
2016-08-06 11:13:38 +0800

    read file function

--- stock_report.py
+++ stock_report.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+def checkFileSize( file ):
+    content = file.read()
+    return (len(content) < 1000 )
 
-def normalize(name):
-    return name.capitalize()
 
-L1 = ['adam', 'LISA', 'barT']
-L2 = list(map(normalize, L1));
+f = open('1.txt', 'r')
 
-print(L2);
+fList = f.readlines()
+department = fList[1][6:]           #营业部名称
+shareholder = fList[2][3:]          #股东
+fundBallance = fList[3][5:14]       #资金余额
+stockMarketValue = fList[4][17:]    #股票市值
+totlaAssets = fList[5][22:]         #总 资 产
+
+
+print(fList[8])
+
+f.close()
+
+
